92acdd8885f9a5cac819e7fc271c7fcf45829845
hadi <hadifanatic1@gmail.com>
2026-06-28 03:50:57 -0700

    chore: add pre-push hook

--- src/components/button/button.js
+++
@@ -1,4 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-export function Button({ children, ...props }) {
-    return (_jsx("button", { ...props, className: "px-5 py-3 cursor-pointer text-white bg-blue-600 rounded-lg", children: children }));
-}
